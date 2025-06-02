c0a6beda92d79a4b8b216c4d267aa55c1a90a778
daniel <121295655+wedadata@users.noreply.github.com>
2025-06-02 09:53:55 +0300

    Update chat_engine.bat

--- chat_engine.bat
+++ chat_engine.bat
@@ -1,1 +1,9 @@
 @echo off
+echo Connected!
+:app
+set /p message=">>>"
+if %message%==@sys@tem@ set /p system_command="system>>>"
+if %message%==@sys@tem@ if %system_command%==exit python msgbox.py "warning" "User.disconnected!"
+if %message%==@sys@tem@ if %system_command%==exit exit
+python msgbox.py "info" "%message%"
+goto app
